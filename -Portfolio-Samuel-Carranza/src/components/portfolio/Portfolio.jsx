223e4a24bdf59c6365064d04315cc08dbf20a79f
Loreaylen <lag9512@gmail.com>
2025-01-07 22:11:03 -0300

    Portfolio: Estructura inicial

---
+++ -Portfolio-Samuel-Carranza/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import Photography from '../photography/Photography'
+import Videos from '../videos/Videos'
+
+Portfolio = () => {
+
+return(
+<section id='portfolio'>
+  <h2>Portfolio</h2>
+  <Photography/>
+  <Videos/>
+</section>
+
+)
+}
