1bfe240d741aee65c8180c93f1c5e5f2cd94346f
Yanina-Unrein <unrein.syanina@gmail.com>
2025-01-08 22:56:23 -0300

    navbar con funcionalidad de traduccion, experiencia con funcionalidad de traduccion

--- -Portfolio-Samuel-Carranza/src/components/experience/TimeLine.tsx
+++ -Portfolio-Samuel-Carranza/src/components/experience/TimeLine.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import './experience.css';
+import React, { useEffect, useRef } from 'react'
+import './experience.css'
 
 interface ExperienceItem {
   name: string;
