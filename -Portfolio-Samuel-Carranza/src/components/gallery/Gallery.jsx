e5606d6e668af68463939b7b616d8254f90c831f
Loreaylen <lag9512@gmail.com>
2025-01-13 21:57:56 -0300

    Porftolio: Se agregó funcionalidad a galería para hacer full width

--- -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
+++ -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
@@ -1,20 +1,34 @@
+import { useState } from 'react'
 import './gallery.css'
 import '../portfolio/portfolio.css'
 import imageData from '../../data/images'
-const Gallery = () => {
 
-  return(
+const Gallery = () => {
+  const [fullWidth, SetFullWidth] = useState(null)
+
+
+  const openFullWidth = (key) => {
+    return SetFullWidth(key)
+  }
+
+  const closeFullWidth = () => {
+    return SetFullWidth(null)
+  }
+
+  return (
     <>
-    <h3>Gallery</h3>
-    <div className="masonry">
-      {
-        imageData.map((img, i) => {
-        const src = '/public/images/portfolio/' + img + '.webp'
-        return <img src={src} alt='imagen' key={i} className='item' />
-        })
-      }
-      
-    </div>
+      <h3>Gallery</h3>
+      <div className="masonry">
+        {
+          imageData.map((img, i) => {
+            const src = '/public/images/portfolio/' + img + '.webp'
+            return <div key={i} className={`item ${fullWidth === i ? 'item-fw' : ''}`}>
+              <img src={src} alt='imagen' className={`item-photo ${fullWidth === i ? 'photo-fw' : ''}`} onClick={() => { openFullWidth(i) }} />
+              <button onClick={closeFullWidth} className={`close-image-icon ${fullWidth === i ? '' : 'invisible'}`} />
+            </div>
+          })
+        }
+      </div>
     </>
   )
 }
