bb88988d922d87f6260d4fc5089413bb1889d3ba
Loreaylen <lag9512@gmail.com>
2025-01-16 22:44:11 -0300

    Gallery: Intento de lazy loading

--- -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
+++ -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
@@ -4,7 +4,11 @@ import '../portfolio/portfolio.css'
 import imageData from '../../data/images'
 
 const Gallery = () => {
-  const [fullWidth, SetFullWidth] = useState(null)
+  const [fullWidth, SetFullWidth] = useState(null),
+    [loaded, setLoaded] = useState(false);
+
+
+
 
   return (
     <>
@@ -12,11 +16,19 @@ const Gallery = () => {
       <div className="masonry">
         {
           imageData.map((img, i) => {
-            const src = '/public/images/portfolio/' + img + '.webp'
-            return <div key={i} className={`item ${fullWidth === i ? 'item-fw' : ''}`}>
-              <img src={src} alt='imagen' className={`item-photo ${fullWidth === i ? 'photo-fw' : ''}`} onClick={() => { SetFullWidth(i) }} />
-              <button onClick={() => {SetFullWidth(null)}} className={`close-image-icon ${fullWidth === i ? '' : 'invisible'}`} />
-            </div>
+            const src = '/public/images/portfolio/' + img + '.webp';
+            return (
+              <div id={i} key={i} className={`item ${fullWidth === i ? 'item-fw' : ''} ${loaded ? '' : 'loading'}`}>
+                    <img src={src}
+                      alt='imagen'
+                      loading='lazy'
+                      className={`item-photo ${loaded ? '' : 'hidden'}${fullWidth === i ? 'photo-fw' : ''}`}
+                      onClick={() => { SetFullWidth(i) }}
+                      onLoad={() => { setLoaded(true) }}
+                      />
+                    <button onClick={() => { SetFullWidth(null) }} className={`close-image-icon ${fullWidth === i ? '' : 'invisible'}`} />
+              </div>
+            )
           })
         }
       </div>
