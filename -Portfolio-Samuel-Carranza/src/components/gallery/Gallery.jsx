a3703f86d114cf4683b008320505e7b8557b0da2
Loreaylen <lag9512@gmail.com>
2025-01-20 23:09:34 -0300

    Galería: Modificando css a slider

--- -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
+++ -Portfolio-Samuel-Carranza/src/components/gallery/Gallery.jsx
@@ -22,24 +22,31 @@ const Gallery = () => {
   return (
     <>
       <h3>{t('titles.gallery')}</h3>
-      <div className="masonry">
-        {
-          imageData.map((img, i) => {
-            const src = '/public/images/portfolio/' + img + '.webp';
-            return (
-              <div id={i} key={i} className={`item ${fullWidth === i ? 'item-fw' : ''} ${loaded ? '' : 'loading'}`}>
-                    <img src={src}
-                      alt={`imagen ${i}`}
-                      loading='lazy'
-                      className={`item-photo ${loaded ? '' : 'hidden'}${fullWidth === i ? 'photo-fw' : ''}`}
-                      onClick={() => { onFullWidth(i) }}
-                      onLoad={() => { setLoaded(true) }}
-                      />
-                    <button onClick={() => { closeFullWidth() }} className={`close-image-icon ${fullWidth === i ? '' : 'invisible'}`} />
-              </div>
-            )
-          })
-        }
+      <div className='masonry-container'>
+        <div className="masonry">
+          {
+            imageData.map((img, i) => {
+              const src = '/public/images/portfolio/' + img + '.webp';
+              return (
+                <div id={i} key={i} className={`item ${fullWidth === i ? 'item-fw' : ''} ${loaded ? '' : 'loading'}`}>
+                      <img src={src}
+                        alt={`imagen ${i}`}
+                        loading='lazy'
+                        className={`item-photo ${loaded ? '' : 'hidden'}${fullWidth === i ? 'photo-fw' : ''}`}
+                        onClick={() => { onFullWidth(i) }}
+                        onLoad={() => { setLoaded(true) }}
+                        />
+                      <button onClick={() => { closeFullWidth() }} className={`close-image-icon ${fullWidth === i ? '' : 'invisible'}`} />
+                </div>
+              )
+            })
+          }
+        </div>
+        <button className='gallery-slider-btn prev-btn'>
+          <img src="/icons/arrow-left-icon.svg" alt="Previous" /></button>
+        <button className='gallery-slider-btn next-btn'>
+        <img src="/icons/arrow-right-icon.svg" alt="Previous" />
+        </button>
       </div>
     </>
   )
