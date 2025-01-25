9e839a0a0e452443b2b445bfe176d972660a9fd3
Loreaylen <lag9512@gmail.com>
2025-01-25 20:19:31 -0300

    Videos: Traducción de mensaje y lógica del fullWidth

--- -Portfolio-Samuel-Carranza/src/components/videos/Videos.jsx
+++ -Portfolio-Samuel-Carranza/src/components/videos/Videos.jsx
@@ -1,25 +1,49 @@
-import './videos.css';
+import { useState } from 'react'
 import videoData from '../../data/videos.js';
+import { useTranslation } from 'react-i18next';
+import './videos.css';
 
 const Videos = () => {
+  const { t } = useTranslation(),
+        [fullWidth, setFullWidth] = useState(null);
+
+  const onOpenVideo = (index) => {
+    setFullWidth(index)
+    document.documentElement.style.overflow = 'hidden';
+    console.log('clickeado fw')
+    return
+  }
+
+  const onCloseVideo = (i) => {
+    setFullWidth(null)
+    document.documentElement.style.overflow = 'auto';
+    console.log('clickeado')
+  }
 
   return (
     <>
       <h3>Videos</h3>
-      <div className="videoContainer">
+      <div className="video-container">
         {videoData.map((video, i) => (
+          <div key={i} className={`video-item-container ${fullWidth === i ? 'video-fw' : ''}`}
           
-          <video key={i}
-            width={200} 
-            height={150} 
-            controls={true} 
-            autoPlay={false}
-            muted
-            preload='metadata'>
+          >
+          <video 
+            className={`video-item ${fullWidth === i ? 'video-item-fw': ''}`}
+            controls={fullWidth === i}
+            preload='metadata'
+            onClick={() => {
+              if(fullWidth === i) return
+              onOpenVideo(i)
+              return
+            }}
+            >
             <source src={'/videos/' + video.webm} type="video/webm" />
             <source src={'/videos/' + video.mp4} type="video/mp4" />
-            Your browser does not support the video tag.
+            {t('videos.notSupport')}
           </video>
+          <button onClick={() => { onCloseVideo(i) }} className={`close-video-icon ${fullWidth === i ? '' : 'invisible'}`} />
+          </div>
         ))}
       </div>
     </>
