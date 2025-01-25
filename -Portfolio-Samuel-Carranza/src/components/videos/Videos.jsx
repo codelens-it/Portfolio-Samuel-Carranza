d229e96e42d33afad27cbb85bb3f738385a025f0
Loreaylen <lag9512@gmail.com>
2025-01-25 20:54:52 -0300

    Videos: pausar video

--- -Portfolio-Samuel-Carranza/src/components/videos/Videos.jsx
+++ -Portfolio-Samuel-Carranza/src/components/videos/Videos.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import videoData from '../../data/videos.js';
 import { useTranslation } from 'react-i18next';
 import './videos.css';
 
 const Videos = () => {
   const { t } = useTranslation(),
-        [fullWidth, setFullWidth] = useState(null);
+    [fullWidth, setFullWidth] = useState(null),
+    videoRef = useRef([])
 
   const onOpenVideo = (index) => {
     setFullWidth(index)
+    console.log(videoRef.current)
     document.documentElement.style.overflow = 'hidden';
-    console.log('clickeado fw')
     return
   }
 
-  const onCloseVideo = (i) => {
+  const onCloseVideo = (index) => {
     setFullWidth(null)
+    videoRef.current[index].pause();
     document.documentElement.style.overflow = 'auto';
-    console.log('clickeado')
+    return
   }
 
   return (
@@ -25,24 +27,28 @@ const Videos = () => {
       <h3>Videos</h3>
       <div className="video-container">
         {videoData.map((video, i) => (
-          <div key={i} className={`video-item-container ${fullWidth === i ? 'video-fw' : ''}`}
-          
+          <div key={i}
+            className={`video-item-container ${fullWidth === i ? 'video-fw' : ''}`}
           >
-          <video 
-            className={`video-item ${fullWidth === i ? 'video-item-fw': ''}`}
-            controls={fullWidth === i}
-            preload='metadata'
-            onClick={() => {
-              if(fullWidth === i) return
-              onOpenVideo(i)
-              return
-            }}
+            <video
+              className={`video-item ${fullWidth === i ? 'video-item-fw' : ''}`}
+              ref={ref => { videoRef.current[i] = ref }}
+              controls={fullWidth === i}
+              preload='metadata'
+              onClick={() => {
+                if (fullWidth === i) return
+                onOpenVideo(i)
+                return
+              }}
             >
-            <source src={'/videos/' + video.webm} type="video/webm" />
-            <source src={'/videos/' + video.mp4} type="video/mp4" />
-            {t('videos.notSupport')}
-          </video>
-          <button onClick={() => { onCloseVideo(i) }} className={`close-video-icon ${fullWidth === i ? '' : 'invisible'}`} />
+              <source src={'/videos/' + video.webm} type="video/webm" />
+              <source src={'/videos/' + video.mp4} type="video/mp4" />
+              {t('videos.notSupport')}
+            </video>
+            <button
+              onClick={() => { onCloseVideo(i) }}
+              className={`close-video-icon ${fullWidth === i ? '' : 'invisible'}`}
+            />
           </div>
         ))}
       </div>
