1f3aa2847187b3dd118cdfe01b4e1d68fe27fad0
Loreaylen <lag9512@gmail.com>
2025-01-25 19:08:15 -0300

    Videos: Creación de videos.js

---
+++ -Portfolio-Samuel-Carranza/src/data/videos.js
@@ -0,0 +1,8 @@
+const videoData = [
+  {webm: 'finalCut4.webm', mp4: 'finalCut4.mp4'},
+  {webm: 'rolling.webm', mp4: 'rolling.mp4'},
+  {webm: 'cadejoBrewingCompany.webm', mp4: 'cadejoBrewingCompany.mp4'},
+  {webm: 'nightcrawler.webm', mp4: 'nightcrawler.mp4'}
+]
+
+export default videoData;
