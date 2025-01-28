e97484be4ea75d621032ef0b781b329caa5a0477
CodeLens <codelens.ar@gmail.com>
2025-01-28 00:30:11 -0300

    Update App.jsx

--- -Portfolio-Samuel-Carranza/src/App.jsx
+++ -Portfolio-Samuel-Carranza/src/App.jsx
@@ -7,13 +7,20 @@ import Footer from './components/footer/Footer'
 import Section from './components/section/Section'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
-
+import {Helmet} from "react-helmet";
+import { useTranslation } from 'react-i18next'
 
 
 function App() {
+  const { t } = useTranslation();
 
+  
   return (
-   <>
+   <> 
+       <Helmet>
+        <html lang={t('titles.lang')} />
+        <meta name="description" content={t('titles.metadescription')} />
+      </Helmet>
       <Navbar />
       <Hero />
       <Section id={'experience'}>
